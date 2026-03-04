155abfe542401eb2aecc212d9f25a445b70a8e26
Kirill Kravchenko <ayrek2015@yandex.ru>
2026-03-04 15:03:13 +0300

    feat: add pages with links and router in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Проверка работы TetraSkill');
+import Router from './router';
+import { HomePage, game1, game2, game3, game4 } from './pages';
+
+const router = new Router('app');
+
+router.addRoute('/', HomePage);
+router.addRoute('/game1', game1);
+router.addRoute('/game2', game2);
+router.addRoute('/game3', game3);
+router.addRoute('/game4', game4);
+
+router.start();
---
+++ src/pages.ts
@@ -0,0 +1,13 @@
+export const HomePage = (): string => {
+  return `
+    <div>
+      <h1>TetraSkill</h1>
+      <ul>
+        <li><a href="/game1">game1</a></li>
+        <li><a href="/game2">game2</a></li>
+        <li><a href="/game3">game3</a></li>
+        <li><a href="/game4">game4</a></li>
+      </ul>
+    </div>
+  `;
+};
